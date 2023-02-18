00c0e13b2b6030d073a2ac21223869c750232ede
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-02-18 11:03:41 +0200

    feat: added update avatar of user

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const controllerWrapper = require("./controllerWrapper");
 const HttpError = require("./HttpError");
 const handleSaveErrors = require("./handleSaveErrors");
+const uploadImage = require("./cloudinary");
 
 module.exports = {
 	controllerWrapper,
 	HttpError,
+	uploadImage,
 	handleSaveErrors,
 };
--- models/auth.js
+++ models/auth.js
@@ -21,6 +21,9 @@ const userSchema = new Schema({
 	token: {
 		type: String,
 		default: ""
+	},
+	avatarUrl: {
+		type: String
 	}
 }, {
 	versionKey: false,
