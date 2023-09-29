f1f9b608d524d343af7dacf374bfea3fca6ff053
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-09-29 22:23:55 +0300

    feat: added default avatar

--- contollers/auth/addUser.js
+++ contollers/auth/addUser.js
@@ -1,23 +1,26 @@
-const { User } = require("../../models/auth");
-const { HttpError } = require("../../helpers");
-const bcrypt = require("bcryptjs");
+const { User } = require("../../models/auth")
+const { HttpError } = require("../../helpers")
+const bcrypt = require("bcryptjs")
+const gravatar = require("gravatar")
 
 const addUser = async (req, res, next) => {
-	const { name, email, password } = req.body;
-	const user = await User.findOne({ email });
+  const { name, email, password, avatar } = req.body
+  const user = await User.findOne({ email })
 
-	if (user) {
-		throw HttpError(409, "Email in use");
-	}
-	
-	const hashedPassword = await bcrypt.hash(password, 10)
-	
-  const newUser = await User.create({name, email, password: hashedPassword});
-	
-	res.status(201).json({
-		name: newUser.name,
-		email: newUser.email,
-	})
+  if (user) {
+    throw HttpError(409, "Email in use")
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const avatarUrl =
+    avatar || gravatar.url(email, { s: "100", r: "x", d: "retro" })
+  const newUser = await User.create({ name, email, password: hashedPassword })
+
+  res.status(201).json({
+    name: newUser.name,
+    email: newUser.email,
+    avatarUrl,
+  })
 }
 
-module.exports = addUser;
+module.exports = addUser
--- contollers/auth/loginUser.js
+++ contollers/auth/loginUser.js
@@ -1,40 +1,39 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { User } = require("../../models/auth");
-const { HttpError } = require("../../helpers");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { User } = require("../../models/auth")
+const { HttpError } = require("../../helpers")
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY } = process.env
 
 const loginUser = async (req, res) => {
-	const { password, email } = req.body;
-	let user = await User.findOne({ email });
-	if (!user) {
-		throw HttpError(401, "Email or password invalid");
-	}
-	
-	const passwordCompare = await bcrypt.compare(password, user.password);
-	
-	if (!passwordCompare) {
-		throw HttpError(401, "Email or password invalid");
-	}
-	
-	const payload = {
-		id: user._id
-	}
-	const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h"});
-	await User.findByIdAndUpdate(user._id, { accessToken });
-	user = await User.findById(user._id, {
-		accessToken,
-		email: 1,
-		phone: 1,
-		_id: 1,
-		name: 1,
-		avatarUrl: 1
-	});
-	
-	res.json({
-		user,
-	});
+  const { password, email } = req.body
+  let user = await User.findOne({ email })
+  if (!user) {
+    throw HttpError(401, "Email or password invalid")
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password)
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid")
+  }
+
+  const payload = {
+    id: user._id,
+  }
+  const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" })
+  await User.findByIdAndUpdate(user._id, { accessToken })
+  user = await User.findById(user._id, {
+    accessToken,
+    email: 1,
+    _id: 1,
+    name: 1,
+    avatarUrl: 1,
+  })
+
+  res.json({
+    user,
+  })
 }
 
-module.exports = loginUser;
+module.exports = loginUser
